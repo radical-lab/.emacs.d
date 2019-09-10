0b562bc878e271c1dc9b3571c7e360da588d78f6
Shyam-Has-Your-Anomaly-Mitigated <shyam@shyam.id.au>
2019-09-11 05:18:01 +1000

    elore

--- init.el
+++ init.el
@@ -21,3 +21,18 @@
 
 ; ##prolog
 (add-to-list 'interpreter-mode-alist '("swipl" . prolog-mode))
+
+; printf \\ec
+; emacs -nw --no-splash --eval='(progn(help-with-tutorial)(goto-line 110))'
+; this stupid standard is just for the shell, and i don't support it either; shells can just check if the output ends with \n, or preppend it to the prompt (some shells already do this, but i can't remember; by shell, i also mean terminal emulator)
+; https://www.gnu.org/software/emacs/manual/html_node/emacs/Customize-Save.html
+; if there are "specific kinds of files that are always supposed to end in" \n; that's just bad language design #idiocracy
+; cat init.el
+(setq      require-final-newline nil)
+(setq mode-require-final-newline nil); if some"-thing" doesn't work: first try `cp cfg/.emacs.d/init.el ~/.emacs.d/init.el`
+
+; /^;[^;]/ is not the Elisp way ∵ *scratch* only has /^;;/, and there's some esoteric law about /[^;];[^;]/ only being inlined after an instruction, reminiscent of the syntax of C; 'tis the UNIX way, reminding us the Elisp Machine Accumulates C Supplements
+(setq electric-indent-mode nil)
+; ERR: goto `emacs -Q -nw`, type ";\t", and it indents the semicolon (alot)
+
+; EOF
